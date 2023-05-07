35c04d6122bcae8184ac305437052a1499a91881
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-05-07 13:44:57 +0300

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,1 +1,1 @@
-apt update && yes | apt upgrade && apt install qemu-user-x86-64 proot-distro -y && mv ./distro/* $PREFIX/etc/proot-distro && cd .. && yes | rm -rR ./linux-x86_64-in-termux && cd .. && clear && echo "Now run proot-distro list and for install do proot-distro install (distro)!"
+apt install qemu-user-x86-64 proot-distro -y && mv ./distro/* $PREFIX/etc/proot-distro && cd .. && yes | rm -rR ./linux-x86_64-in-termux && cd .. && clear && echo "Now run proot-distro list and for install do proot-distro install (distro)!"
