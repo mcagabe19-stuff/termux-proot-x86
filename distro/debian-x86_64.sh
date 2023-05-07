16300df413ed95bb94530cfbe46a57e7bfe68ebb
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-05-07 12:07:29 +0300

    Create debian-x86_64.sh

---
+++ distro/debian-x86_64.sh
@@ -0,0 +1,6 @@
+DISTRO_NAME="Debian x86_64"
+DISTRO_COMMENT="A stable release (bullseye) x86_64 for AArch64"
+
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-x86_64-pd-v3.10.0.tar.xz"
+TARBALL_SHA256['x86_64']="1957c76d4013d16f44eebfd9cc5b0074f421b5ea473e2a36c15d2939a802e339"
+DISTRO_ARCH="x86_64"
