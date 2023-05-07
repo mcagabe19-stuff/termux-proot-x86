e902c375cc046a3c5f438b8067f0cf174162c3fc
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-05-07 12:07:04 +0300

    Create ubuntu-x86_64.sh

---
+++ distro/ubuntu-x86_64.sh
@@ -0,0 +1,6 @@
+DISTRO_NAME="Ubuntu x86_64"
+DISTRO_COMMENT="Current LTS release (jammy). Modified for x86_64"
+
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/ubuntu-x86_64-pd-v3.10.0.tar.xz"
+TARBALL_SHA256['x86_64']="51a1da5b4db87ec35853d0865b1d7bf2472d39007f698ecd7ae4fa68edeb700b"
+DISTRO_ARCH="x86_64"
