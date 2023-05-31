1491f8da1678c91b5b3f1a50ce5714db75b273b5
Gabe <82843871+mcagabe19@users.noreply.github.com>
2023-05-31 22:10:23 +0300

    void update x86_64

--- distrox86_64/void-x86_64.sh
+++ distrox86_64/void-x86_64.sh
@@ -1,10 +1,12 @@
 DISTRO_NAME="Void Linux x86_64"
 
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.5.1/void-x86_64-pd-v3.5.1.tar.xz"
-TARBALL_SHA256['x86_64']="dc1bd6ce37f3668565310b23b0ab2181ae33ea680211b2541cce896b03c45644"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.11.0/void-x86_64-pd-v3.11.0.tar.xz"
+TARBALL_SHA256['x86_64']="676546678dc2fdfc4e5ffa5c3dc8b3aa5dc8b884108fdf2346f4edd0311679bc"
 DISTRO_ARCH="x86_64"
 
 distro_setup() {
 	# Set default shell to bash.
 	run_proot_cmd usermod --shell /bin/bash root
+	# Fix issue where come CA certificates links may not be created.
+	run_proot_cmd update-ca-certificates --fresh
 }
