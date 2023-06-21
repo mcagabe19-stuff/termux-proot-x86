010adc67c5aa64401aa1395e03fb7b5f63da28f2
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-06-21 19:00:59 +0300

    debian update

--- distroi386/debian-i386.sh
+++ distroi386/debian-i386.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="Debian i386"
 DISTRO_COMMENT="A stable release (bullseye)."
 
-TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-i686-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['i686']="11bb786b36ea0d1a0177f1eb8c2a8fea4916675dc1cf347a3556c61d89d7e54c"
+TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v3.12.1/debian-i686-pd-v3.12.1.tar.xz"
+TARBALL_SHA256['i686']="548f009afd88982cdf55e4bda1d30594add36e91425254b360613b270908cfed"
 DISTRO_ARCH="i686"
--- distrox86_64/debian-x86_64.sh
+++ distrox86_64/debian-x86_64.sh
@@ -2,5 +2,5 @@ DISTRO_NAME="Debian x86_64"
 DISTRO_COMMENT="A stable release (bullseye)."
 
 TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-x86_64-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['x86_64']="1957c76d4013d16f44eebfd9cc5b0074f421b5ea473e2a36c15d2939a802e339"
+TARBALL_SHA256['x86_64']="00dd9cc73359500028e1268a6bf6819a33161ba48f7b110669b8b2db022f1322"
 DISTRO_ARCH="x86_64"
