53486ed75569ca0b342e2b059a35b9029187a57c
mcagabe19 <egzozu.be.bas@gmail.com>
2023-07-10 16:46:19 +0300

    fedora update and oop

--- distrox86_64/debian-x86_64.sh
+++ distrox86_64/debian-x86_64.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="Debian x86_64"
 DISTRO_COMMENT="A stable release (bullseye)."
 
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-x86_64-pd-v3.10.0.tar.xz"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.12.1/debian-x86_64-pd-v3.12.1.tar.xz"
 TARBALL_SHA256['x86_64']="00dd9cc73359500028e1268a6bf6819a33161ba48f7b110669b8b2db022f1322"
 DISTRO_ARCH="x86_64"
--- distrox86_64/fedora-x86_64.sh
+++ distrox86_64/fedora-x86_64.sh
@@ -1,5 +1,5 @@
-DISTRO_NAME="Fedora 37 x86_64"
+DISTRO_NAME="Fedora 38 x86_64"
 
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.10.0/fedora-x86_64-pd-v3.10.0.tar.xz"
-TARBALL_SHA256['x86_64']="e6386687148325a07f8822a2091a859e35661c79300360d498bf5f764ab22ed0"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v3.15.2/fedora-x86_64-pd-v3.15.2.tar.xz"
+TARBALL_SHA256['x86_64']="167732ad9389523ca88ca9fdba470413322be15b3110845fa865e17b81e3ffaa"
 DISTRO_ARCH="x86_64"
