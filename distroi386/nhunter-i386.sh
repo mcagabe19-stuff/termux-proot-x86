18ae2d648aae3ba321ae1dd406548df8be101ba2
mcagabe19 <egzozu.be.bas@gmail.com>
2023-07-17 16:37:40 +0300

    ugh I needed workflow for this

--- distroi386/nhunter-i386.sh
+++ distroi386/nhunter-i386.sh
@@ -2,6 +2,6 @@ DISTRO_NAME="Nethunter i386"
 DISTRO_COMMENT="Kali Linux Minimal"
 
 TARBALL_URL['i686']="https://kali.download/nethunter-images/current/rootfs/kalifs-i386-minimal.tar.xz"
-TARBALL_SHA256['i686']="e83cd8f57d6128efd64e88b191a1653ff315fffd78c05d536d2b6f63b2e6d49d"
+TARBALL_SHA256['i686']="0dd4b95635b601b9516c29bee016363dd5f378f9abb104736f93f76c82c3d8b6"
 
 DISTRO_ARCH="i686"
--- distrox86_64/nhunter-x86_64.sh
+++ distrox86_64/nhunter-x86_64.sh
@@ -2,6 +2,6 @@ DISTRO_NAME="Nethunter x86_64"
 DISTRO_COMMENT="Kali Linux Minimal"
 
 TARBALL_URL['x86_64']="https://kali.download/nethunter-images/current/rootfs/kalifs-amd64-minimal.tar.xz"
-TARBALL_SHA256['x86_64']="096290b7229ab81f1ac3b35324a7109dc19f1e2f5bf6aab1ff8254ebc95463ea"
+TARBALL_SHA256['x86_64']="b7cfeccdcbf7a2844245405ecc680432237d3b0da49ee35eab0f3ce9fab645a1"
 
 DISTRO_ARCH="x86_64"
