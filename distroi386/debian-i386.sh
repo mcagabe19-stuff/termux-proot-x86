e16250dda9bda6170df4e45e20e34c346c90fb36
mcagabe19 <egzozu.be.bas@gmail.com>
2023-07-17 16:22:56 +0300

    bump

--- distroi386/debian-i386.sh
+++ distroi386/debian-i386.sh
@@ -1,5 +1,5 @@
 DISTRO_NAME="Debian i386"
-DISTRO_COMMENT="A stable release (bullseye)."
+DISTRO_COMMENT="A stable release (bookworm)."
 
 TARBALL_URL['i686']="https://github.com/termux/proot-distro/releases/download/v3.12.1/debian-i686-pd-v3.12.1.tar.xz"
 TARBALL_SHA256['i686']="548f009afd88982cdf55e4bda1d30594add36e91425254b360613b270908cfed"
