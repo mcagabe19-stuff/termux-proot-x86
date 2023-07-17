f2acea53b25efc2d031383018af6402a19cc9192
mcagabe19 <egzozu.be.bas@gmail.com>
2023-07-17 16:58:04 +0300

    nah will bump when release comes

--- distroi386/nhunter-i386.sh
+++ distroi386/nhunter-i386.sh
@@ -1,7 +1,7 @@
 DISTRO_NAME="Nethunter i386"
 DISTRO_COMMENT="Kali Linux Minimal"
 
-TARBALL_URL['i686']="https://kali.download/nethunter-images/current/rootfs/kalifs-i386-minimal.tar.xz"
+TARBALL_URL['i686']="https://kali.download/nethunter-images/kali-2023.2/rootfs/kalifs-i386-minimal.tar.xz"
 TARBALL_SHA256['i686']="0dd4b95635b601b9516c29bee016363dd5f378f9abb104736f93f76c82c3d8b6"
 
 DISTRO_ARCH="i686"
