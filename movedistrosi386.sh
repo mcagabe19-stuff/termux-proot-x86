c3cbff2d05ccbd36b1ae8a5325ae1fcea0be5e30
Lily <egzozu.be.bas@gmail.com>
2024-02-08 16:26:04 +0300

    oops

--- movedistrosi386.sh
+++
@@ -1,1 +0,0 @@
-cp ./distros-i686/* $PREFIX/etc/proot-distro && dos2unix $PREFIX/etc/proot-distro/*-i386.sh > /dev/null && echo "Now run proot-distro list and for install do proot-distro install (distro)!"
