074e19ceb125b0779ae3e6aef1ff6e78bb34bf88
Lily <egzozu.be.bas@gmail.com>
2024-02-08 15:02:20 +0300

    Update nhunter-nano.sh

--- customdistros/nhunter/distro/nhunter-nano.sh
+++ customdistros/nhunter/distro/nhunter-nano.sh
@@ -1,7 +1,7 @@
 DISTRO_NAME="Nethunter Nano"
 DISTRO_COMMENT="Kali Linux Nano (aarch64 and arm only)"
 
-TARBALL_URL['aarch64']="https://kali.download/nethunter-images/kali-2023.2/rootfs/kalifs-arm64-nano.tar.xz"
-TARBALL_SHA256['aarch64']="04a12b79d4ac38b7c85ea3eb25a85bff07fb89505a889782a114a97a5de2a84b"
-TARBALL_URL['arm']="https://kali.download/nethunter-images/kali-2023.2/rootfs/kalifs-armhf-nano.tar.xz"
-TARBALL_SHA256['arm']="17bae20743c67017d83448bc82de7cedaeb354d1c75dbadf2357fe93cf769f84"
+TARBALL_URL['aarch64']="https://kali.download/nethunter-images/kali-2023.4/rootfs/kalifs-arm64-nano.tar.xz"
+TARBALL_SHA256['aarch64']="44e70a16ddf3e94d5a27e209c8d643c556ec674d556fe8dc33fbd63bed220d71"
+TARBALL_URL['arm']="https://kali.download/nethunter-images/kali-2023.4/rootfs/kalifs-armhf-nano.tar.xz"
+TARBALL_SHA256['arm']="e9a32ff1d1baf50a5060a151821341c997abe4340bebcf8b18af31d1f41a3da3"
