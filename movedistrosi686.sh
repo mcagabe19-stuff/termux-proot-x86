eabcd7af7bb1095458186306f56e9e97e6b31845
Lily <egzozu.be.bas@gmail.com>
2024-02-08 16:27:06 +0300

    dkqodkwoe

--- movedistrosi686.sh
+++ movedistrosi686.sh
@@ -1,1 +1,1 @@
-cp ./distros-i686/* $PREFIX/etc/proot-distro && dos2unix $PREFIX/etc/proot-distro/*-i386.sh > /dev/null && echo "Now run proot-distro list and for install do proot-distro install (distro)!"
+cp ./distros-i686/* $PREFIX/etc/proot-distro && dos2unix $PREFIX/etc/proot-distro/*-i686.sh > /dev/null && echo "Now run proot-distro list and for install do proot-distro install (distro)!"
