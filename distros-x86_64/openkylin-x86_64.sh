ec351967f3c996558dc652dff14c5c0ffc8a7059
Lily <egzozu.be.bas@gmail.com>
2024-04-12 21:11:17 +0300

    update openkylin

--- distros-x86_64/openkylin-x86_64.sh
+++ distros-x86_64/openkylin-x86_64.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="OpenKylin x86_64"
-DISTRO_COMMENT="Version 'yangtze'."
+DISTRO_COMMENT="Version 'nile'."
 DISTRO_ARCH="x86_64"
 
-TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.6.0/openkylin-x86_64-pd-v4.6.0.tar.xz"
-TARBALL_SHA256['x86_64']="03f43ab9a38ae44ffee06ca6c950a60814c00d8f202d487e95858987f6ee6cbe"
+TARBALL_URL['x86_64']="https://github.com/termux/proot-distro/releases/download/v4.10.0/openkylin-x86_64-pd-v4.10.0.tar.xz"
+TARBALL_SHA256['x86_64']="d3505c0301636850a9fd018349808b1c0735770d222db99b2eda53602fe8cc17"
